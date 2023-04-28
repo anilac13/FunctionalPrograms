32761b24103aae471a1134f97cc588d9ac654371
anilac13 <anil4u2496@gmail.com>
2023-04-28 15:48:54 +0530

    [Anil] Add:Flip a coin and calculate percentage of Heads, Tails

---
+++ FunctionalPrograms/FlipCoin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class FlipCoin
+    {
+        public static void CalculatePercentage()
+        {
+            Console.WriteLine("\nPlease enter number of times to flip a coin");
+            int flipCoin = Convert.ToInt32(Console.ReadLine());
+            if (flipCoin > 0)
+            {
+                int headsCount = 0, tailsCount = 0;
+                for (int i=1; i<=flipCoin; i++)
+                {
+                    Random random = new Random();
+                    double result = random.NextDouble();
+                    if (result > 0.5)
+                    {
+                        headsCount++;
+                    }
+                    if (result < 0.5)
+                    {
+                        tailsCount++;
+                    }
+                }
+                Console.WriteLine("Heads count = {0} times", headsCount);
+                Console.WriteLine("Tails count = {0} times", tailsCount);
+                double headsPercentage = headsCount * 100 / flipCoin;
+                double tailsPercentage = tailsCount * 100 / flipCoin;
+                Console.WriteLine("Heads percentage = {0}%", headsPercentage);
+                Console.WriteLine("Tails percentage = {0}%", tailsPercentage);
+            }
+            else
+            {
+                Console.WriteLine("Enter positive integer only");
+            }
+        }
+    }
+}
--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -5,7 +5,21 @@ namespace FunctionalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nChoose an option to perform \n1.Flip Coin \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin.CalculatePercentage();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
