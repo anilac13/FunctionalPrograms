ec3522f7b682e6a387d05118a3a5815d7c219afb
anilac13 <anil4u2496@gmail.com>
2023-04-29 18:28:29 +0530

    [Anil] Add:Check entered character is Vowel or Consonant

---
+++ FunctionalPrograms/VowelsOrConsonants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class VowelsOrConsonants
+    {
+        public static void FindVowelOrConsonant()
+        {
+            Console.WriteLine("Enter any character");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a'|| ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("{0} is Vowel", ch);
+            }
+            else if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine("{0} is Consonant", ch);
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid alphabet");
+            }
+        }
+    }
+}
